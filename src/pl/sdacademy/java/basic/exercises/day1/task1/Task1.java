06c22eb7d530b420842f1347eea2af1341efdfd7
Pawel Dudko <paweldudko@outlook.com>
2022-02-19 10:48:06 +0100

    task1

---
+++ src/pl/sdacademy/java/basic/exercises/day1/task1/Task1.java
@@ -0,0 +1,38 @@
+package pl.sdacademy.java.basic.exercises.day1.task1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        //displayWordUsingDecimalFormat();
+        //displayWordUsingHexFormat();
+        displayWordUsingBinaryFormat();
+    }
+
+    private static void displayWordUsingDecimalFormat() {
+        char s = 83;
+        char d = 68;
+        char a = 65;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+    private static void displayWordUsingHexFormat() {
+        char s = 0x53;
+        char d = 0x44;
+        char a = 0x41;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+    private static void displayWordUsingBinaryFormat() {
+        char s = 0b01010011;
+        char d = 0b01000100;
+        char a = 0b01000001;
+        System.out.print(s);
+        System.out.print(d);
+        System.out.print(a);
+    }
+
+}
