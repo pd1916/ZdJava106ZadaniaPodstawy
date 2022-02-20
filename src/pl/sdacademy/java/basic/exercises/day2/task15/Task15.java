68f59f0afe341395886a8d19a6c7c27f5a947097
Pawel Dudko <paweldudko@outlook.com>
2022-02-20 16:05:26 +0100

    day2_task15

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task15/Task15.java
@@ -0,0 +1,26 @@
+package pl.sdacademy.java.basic.exercises.day2.task15;
+
+import java.util.Arrays;
+
+public class Task15 {
+
+    public static void main(String[] args) {
+        String[] firstArray = {"Ala", "ma", "kota"};
+        String[] secondArrayArray = {"Ala", "maa", "kota"};
+        System.out.println("First array: " + Arrays.toString(firstArray));
+        System.out.println("Second array: " + Arrays.toString(secondArrayArray));
+        System.out.println("Result: " + areEqual(firstArray, secondArrayArray));
+    }
+
+    private static boolean areEqual(String[] firstArray, String[] secondArrayArray) {
+        if(firstArray.length != secondArrayArray.length) {
+            return false;
+        }
+        for(int i = 0; i < firstArray.length; i++) {
+            if(!firstArray[i].equals(secondArrayArray[i])) {
+                return false; // pierwsza różnica, od razu kończymy pętle i metoda zwraca false
+            }
+        }
+        return true;
+    }
+}
