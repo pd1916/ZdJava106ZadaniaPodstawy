9f93adb95515f142e7ad714e96e4f09653ae2053
Pawel Dudko <paweldudko@outlook.com>
2022-02-20 12:02:32 +0100

    day2_task4

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task4/Task4.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java.basic.exercises.day2.task4;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+        String input = "Ala ma, kota a kot, ma Ale";
+        System.out.println("Input: " + input);
+        System.out.println("After modifications: " + replace(input));
+    }
+
+    private static String replace(String input) {
+        if(input != null) {
+            return input
+                    .replace(".", "-STOP-")
+                    .replace(",", "-STOP-");
+        }
+        return input;
+    }
+}
