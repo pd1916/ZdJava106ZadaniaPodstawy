f0775c75976113793f15cadb8a8a6fbde35c6a95
Pawel Dudko <paweldudko@outlook.com>
2022-02-20 10:58:03 +0100

    day2_task1

---
+++ src/pl/sdacademy/java/basic/exercises/day2/Task1/Task1.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.java.basic.exercises.day2.Task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String input = "ala";
+        System.out.println("Original word: " + input);
+        System.out.println("Last char: " + getLastCharFromString(input));
+
+        input = "domek";
+        System.out.println("Original word: " + input);
+        System.out.println("Last char: " + getLastCharFromString(input));
+    }
+
+    private static char getLastCharFromString(String input) {
+        int lastPosition = input.length() - 1;
+        //return input.substring(lastPosition);
+        return input.charAt(lastPosition);
+    }
+}
