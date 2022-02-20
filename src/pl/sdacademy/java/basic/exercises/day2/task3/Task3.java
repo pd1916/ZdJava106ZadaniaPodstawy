cd28f736a2e6a573a4c6606f530c44b613a4e20d
Pawel Dudko <paweldudko@outlook.com>
2022-02-20 11:47:36 +0100

    day2_task3

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task3/Task3.java
@@ -0,0 +1,32 @@
+package pl.sdacademy.java.basic.exercises.day2.task3;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        String input = "Ala ma kota";
+
+        String word = "ma";
+        System.out.println("Input: " + input);
+        System.out.println("Word: " + word);
+        System.out.println("Result: " + getIndex(input, word));
+
+        System.out.println();
+
+        word = "";
+        System.out.println("Input: " + input);
+        System.out.println("Word: " + word);
+        System.out.println("Result: " + getIndex(input, word));
+    }
+
+    private static int getIndex(String input, String word) {
+        //if(input != null && word != null && !input.isBlank() && !word.isBlank()) {
+        if(isValid(input) && isValid(word)) {
+            return input.contains(word) ? input.indexOf(word) : -1;
+        }
+        return -1;
+    }
+
+    private static boolean isValid(String text) {
+        return text != null && !text.isBlank();
+    }
+}
