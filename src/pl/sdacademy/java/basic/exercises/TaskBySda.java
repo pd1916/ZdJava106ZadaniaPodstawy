232d7c877636a2e138ef38b9618334420175bf5c
Pawel Dudko <paweldudko@outlook.com>
2022-02-26 17:53:40 +0100

    solution to task from sda

--- src/pl/sdacademy/java/basic/exercises/TaskBySda.java
+++ src/pl/sdacademy/java/basic/exercises/TaskBySda.java
@@ -2,6 +2,25 @@ package pl.sdacademy.java.basic.exercises;
 
 public class TaskBySda {
 
+/*
+Napisz program, który dla podanej liczby (jako String) mnoży jej cyfry tak długo, aż wynik będzie jednocyfrowy. Np.
+    Przykład dla "123".
+            123
+            123 → 1x2x3 = 6
+    Przykład dla "277777788888899".
+            277777788888899
+            277777788888899 → 2x7x7x7x7x7x7x8x8x8x8x8x8x9x9 = 4996238671872
+            4996238671872 → 4x9x9x6x2x3x8x6x7x1x8x7x2 = 438939648
+            438939648 → 4x3x8x9x3x9x6x4x8 = 4478976
+            4478976 → 4x4x7x8x9x7x6 = 338688
+            338688 → 3x3x8x6x8x8 = 27648
+            27648 → 2x7x6x4x8 = 2688
+            2688 → 2x6x8x8 = 768
+            768 → 7x6x8 = 336
+            336 → 3x3x6 = 54
+            54 → 5x4 = 20
+            20 → 2x0 = 0
+*/
     public static void main(String[] args) {
         String numberToReduce = "277777788888899";
         int reducedValue = reduce(numberToReduce);
