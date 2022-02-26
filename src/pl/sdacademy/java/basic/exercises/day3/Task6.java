f0f32ecd5c5eeccd7750832ed3d34b0a294b0209
Pawel Dudko <paweldudko@outlook.com>
2022-02-26 12:40:42 +0100

    day_task6_partb

--- src/pl/sdacademy/java/basic/exercises/day3/Task6.java
+++ src/pl/sdacademy/java/basic/exercises/day3/Task6.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Task6 {
     private static final String INPUT_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
@@ -20,7 +21,23 @@ public class Task6 {
 
         LocalDateTime now = LocalDateTime.now();
 
-        long daysToNextLesson = Duration.between(now, nextLesson).toDays();
-        System.out.println("You have " + daysToNextLesson + " days to the next lesson");
+        // punkt A
+//        long daysToNextLesson = Duration.between(now, nextLesson).toDays();
+//        System.out.println("You have " + daysToNextLesson + " days to the next lesson");
+
+        //punkt B
+        long millis = Duration.between(now, nextLesson).toMillis();
+        System.out.println("Milis: " + millis);
+
+        long days = TimeUnit.MILLISECONDS.toDays(millis);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis));
+        long mins = TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis));
+        long secs = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis));
+
+        System.out.println("days: " + days);
+        System.out.println("hours: " + hours);
+        System.out.println("mins: " + mins);
+        System.out.println("secs: " + secs);
+
     }
 }
